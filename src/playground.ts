a6deb7fe9cb6faa64b63bb17c039cdad05152b68
Abdullah Joyia <abdullahjoyia502@gmail.com>
2026-08-19 01:14:48 +0500

    Add playground.ts with initial TypeScript code and Person type definition

---
+++ src/playground.ts
@@ -0,0 +1,40 @@
+let title: string = 'Hello World'
+
+let count: number
+
+let ready: boolean = true
+
+title = 'New Person'
+
+count = 20000
+
+let message: string = 'hello world'
+
+let prices: string[] = ['hello', 'world']
+
+type Person = {
+    address: string,
+    city: string,
+    age: number,
+    isActive: boolean
+}
+
+const person1: Person = {
+    address: '123 Main St',
+    city: 'Anytown',
+    age: 13,
+    isActive: true
+}
+
+let person2: Person = {
+    address: '123 Bolevarda',
+    city: 'hamilton',
+    age: 13,
+    isActive: true
+}
+
+console.log(count)
+
+console.log(person2)
+
+console.log(prices[0] + ' ' + prices[1])
